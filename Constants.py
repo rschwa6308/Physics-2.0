54c82a1101738f780c1400db4e9cc52e847b0120
StardustGogeta <liam.gerst@gmail.com>
2017-04-04 17:55:33 -0400

    Altering window dimensions
    This now initializes pygame in the constants.py file. This is not the
    best of layouts, but it allows the program to detect total monitor size
    and adjust the window dimensions accordingly.

--- Constants.py
+++ Constants.py
@@ -1,6 +1,10 @@
+from pygame import init as pygameInit
+from pygame.display import Info as WindowDims
 G = 0.5
 
 Density = 0.1
 
-# define starting window width and height
-width, height = 1400, 1200
+# Set window to half of monitor dimensions
+pygameInit()
+info = WindowDims()
+width, height = info.current_w // 2, info.current_h // 2
