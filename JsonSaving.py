bf72a60a4899ca8b227852c9aa0df4b6db62eeea
StardustGogeta <liam.gerst@gmail.com>
2017-05-14 10:05:57 -0400

    Taking away test code
    Also, fixing some capitalization in the file dialog menu.

--- JsonSaving.py
+++ JsonSaving.py
@@ -1,31 +1,5 @@
 import json
 
-
-# test_save = {}
-# test_save["settings"] = {
-#     "G": 100,
-#     "time factor": 1,
-#     "camera": {
-#         "position": (0, 0),
-#         "scale factor": 1
-#     }
-# }
-#
-# test_save["bodies"] = [
-#     {
-#         "mass": 50,
-#         "radius": 50,
-#         "position": (132.5, -784.923423),
-#         "velocity": (324, 4),
-#         "density": 0.5,
-#         "color": (123, 123, 123),
-#         "name": "test body"
-#     }
-# ]
-
-
-
-
 class Save:
     def __init__(self, settings_window):
         grav = settings_window.get_gravity()
